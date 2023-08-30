0532be1e02106f78b6a9af95628c376dbad78a41
ArmandoCaliari <bu.caliari@gmail.com>
2023-08-29 22:38:34 -0300

    props e state

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,21 @@
+import CardFilme from "@/components/CardFilme";
 import Title from "@/components/Title";
 
 export default function Home() {
+
+  const filmes = [
+    {
+      titulo: "Megatubarão 2",
+      nota: 7.3,
+      poster: "https://www.themoviedb.org/t/p/w220_and_h330_face/8tBhAn6qVRQzf5yvEcxjgPMgTkw.jpg"
+    },
+    {
+      titulo: "Barbbie",
+      nota: 2.1,
+      poster: "https://www.themoviedb.org/t/p/w220_and_h330_face/yRRuLt7sMBEQkHsd1S3KaaofZn7.jpg"
+    }
+  ]
+
   return (
     /*<> é a tag fragment só pra nao */
     <>
@@ -24,17 +39,20 @@ export default function Home() {
       </nav>
       <Title>em alta</Title>
 
-      <div id="card" className="flex flex-col w-40 justify-center items-center m-2">
-        <img className="rounded" src="https://place-hold.it/150x220/600/fff" alt="" />
-        <span className="font-bold text-center line-clamp-1" >Titulo do filme do card</span>
-        <div>
-          <span>6.0</span>
-        </div>
-        <a href="" className="bg-pink-600 py-2 w-full rounded text-center">Detalhes</a>
-      </div>
+
+      <section className="flex flex-wrap">
+        {filmes.map(filme => <CardFilme filme={filme}/> )}
+      </section>
+      {/* <section className="flex flex-wrap">
+        {filmes.map((filme) => {
+          return <CardFilme filme={filme}/>
+        })}
+      </section> */}
       <Title>lançamentos</Title>
       <Title>favoritos</Title>
       
     </>
   )
 }
+
+// Ele usou o heroicons
